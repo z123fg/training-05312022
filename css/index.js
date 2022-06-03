0ac574ff93ea275278df046d6f5cf4ce0b89f525
AdamZhou1029 <yizhouzhou@yizhous-MacBook-Pro.local>
2022-06-02 20:15:52 -0500

    css

---
+++ css/index.js
@@ -0,0 +1,19 @@
+/* 
+    css specificity: inline > external inline > internal, 
+    selectors: id > class id>element, union, direct child, descendent, sibling, pseudo selector
+            color
+            background
+            display: inline, block, inline-block, grid(2 dimension), none(visibility:hidden), inline-flex,flex
+            position:fixed relative absolute sticky static(default), absolute position will relocate the element related to its nearest parent element with non static position
+            box-model
+            box-sizing: border-box content-box
+            inset
+            width height
+            padding margin padding-left padding-top..
+            border outline
+            font font-size font-color
+            z-index
+            flexbox: one dimensional layout system
+            align-items:secondary axis
+            justify-content:main axis
+*/
