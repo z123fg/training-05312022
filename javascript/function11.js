675e330de6993e5d90f28df909cd065c95fce09d
aleimew <alex@amoore.com>
2022-06-06 11:27:40 -0400

    did function 11

--- javascript/function11.js
+++ javascript/function11.js
@@ -1,14 +1,30 @@
 /**fucntion 11: */
 function secTopAndBottom(x)
 {
-    var secLow = 0;
-    var secHig = 0;
-    var low = 0;
-    var high = 0;
-
-    for(var i= 0; i < x.Array().length; i++){
-        
+    var arry = x; 
+    for(var i = 0; i < arry.length; i++)
+    {
+        for (var j = 0; j < (arry.length -i -1); j++)
+        {
+            if (arry[j] > arry[j+1])
+            {
+                var temp = arry[j];
+                arry[j] = arry[j+1];
+                arry[j+1] = temp;
+            }
+        }
     }
+
+    var secLow = arry[1];
+    var secHigh = arry[arry.length-2];
+
+    console.log(`Array: ${arry}`);
+    console.log(`The second lowest value is ${secLow}.`);
+    console.log(`The second highest value is ${secHigh}.`);
 }
 
-/**to come back to later. */
+console.log("function 11: ");
+var x = [1,2,3,4,5];
+var y = [8, 18, 35, 2, 458, 29, 383, 593, 28, 99];
+secTopAndBottom(x);
+secTopAndBottom(y);
