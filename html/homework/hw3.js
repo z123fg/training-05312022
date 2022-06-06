d4bb17b890aedcbd04b3a1f1f3fe6cf71b84933b
Xiaohei2021 <hualiang.tian@gmail.com>
2022-06-06 13:55:09 -0400

    solving probs
    solving hw probs

--- html/homework/hw3.js
+++ html/homework/hw3.js
@@ -17,7 +17,47 @@ function palindrome(str){
 }
 
 function wordGenerator(str){
+    let newWord = [];
+    for (let i = 0; i<str.length; i++){
+        for(let n=0; n<str.length+1; n++) {
+            newWord.push(str.slice(i,j));
+        }
+    }
+    return newWord;
+}
 
-    let split = str.split("")
-    
+function capitlaize(str){
+    return str.charAt(0).toUpperCase().str.slice(1);
+}
+
+function longestWord(str){
+    let words = str.split("")
+    for(let i =0; i<words.length; i++){
+        longest = words[0]
+        if(words[i].length>longest.length){
+            longest = words[i];
+            return longest;
+        }
+    }
+}
+
+function vowelCounter(str){
+    const vowels = [a,e,i,o,u]
+    let count = 0;
+
+    for(let letter of str.toLowerCase()){
+        if(vowels.includes(letter)){
+            count++;
+        }
+    }
+    return count;
+}
+
+function primeCheck(num){
+    if(num < 1 && num%2 !=0 ){
+       return true; 
+    }
+    else{
+        return false
+    }
 }
