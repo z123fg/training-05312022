a9ee6679c7a48fc8f4dfe1be531d8b170b77e116
aleimew <alex@amoore.com>
2022-06-06 12:03:55 -0400

    did function 13

--- javascript/function13.js
+++ javascript/function13.js
@@ -1,5 +1,22 @@
 /**function 13 */
-function positiveInteger(){
+function posFactors(x){
+    var factors = [];
+    var posInt = x;
 
+    for(var i = 1; i<= posInt; i++)
+    {
+        if (posInt % i === 0)
+        {
+            factors.push(i);
+        }
+    }
+    console.log(factors);
 }
-/**to come back to later. */
+
+console.log("function 13: ");
+var x = 15;
+var y = 36;
+var z = 79;
+posFactors(x);
+posFactors(y);
+posFactors(z);
