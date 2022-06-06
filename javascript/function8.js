dda3d0f449196f3278bae1d2b474317a53945002
aleimew <alex@amoore.com>
2022-06-06 10:42:19 -0400

    Did function 8

--- javascript/function8.js
+++ javascript/function8.js
@@ -1,5 +1,28 @@
 /*function 8 */
 function primeNumber(x){
-    var isPrime = true;
+    var isPrime = false;
+
+    if (x % 2 != 0 &&
+        x % 3 != 0 &&
+        x % 4 != 0)
+        {
+            isPrime = true;
+        }
+    
+    if (isPrime === true)
+    {
+        console.log(`${x} is a prime number`);
+    }
+    else
+    {
+        console.log(`${x} is NOT a prime number`);
+    }
 }
-/**to come back to later */
+
+console.log("function 8: ");
+var x = 7;
+var y = 14;
+var z = 97;
+primeNumber(x);
+primeNumber(y);
+primeNumber(z);
