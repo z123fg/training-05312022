1e014603961bc5ffedb4b35f47b17e5535ed2c45
aleimew <alex@amoore.com>
2022-06-06 11:05:19 -0400

    Did function 10

--- javascript/function10.js
+++ javascript/function10.js
@@ -1,6 +1,29 @@
 /**function 10: */
-function matrix(x)
+function matrix(x, y)
 {
+    if (x >= 1 && y >= 1)
+    {
+        var rows = x;
+        var columns = y;
+        var m = [];
 
+        for (var i = 0; i < rows; i++)
+        {
+            m[i] = [];
+            for (var j = 0; j < columns; j++)
+            {
+                m[i][j] = Math.floor(Math.random() * 2);
+            }
+        }
+        console.log(m);
+    }
+    else
+    {
+        console.log("insufficent size");
+    }
 }
-/**to come back to later*/
+
+console.log("function 10: ");
+matrix(3, 3);
+matrix(6, 4);
+matrix(0, 0);
