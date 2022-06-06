23e3581191e985635bfae65bc18d2b1b58231ce2
aleimew <alex@amoore.com>
2022-06-06 11:43:12 -0400

    did function 12

--- javascript/function12.js
+++ javascript/function12.js
@@ -1,5 +1,36 @@
 /**function 12 */
-function perfectNumber(){
+function perfectNumber(x){
+    var isPerfect = false;
+    var check = 0;
+    var num = x;
 
+    for (var i = 1; i<=num/2; i++)
+    {
+        if (num%i === 0)
+        {
+            check += i;
+            //console.log(check);
+        }
+    }
+
+    if (check === num && check !== 0)
+    {
+        isPerfect = true;
+    }
+
+    if (isPerfect == true)
+    {
+        console.log(`${num} is a perfect number`);
+    }
+    else{
+        console.log(`${num} is NOT a perfect number`);
+    }
 }
-/**to come back to later. */
+
+console.log("function 12: ")
+var x = 28;
+var y = 512;
+var z = 8128
+perfectNumber(x);
+perfectNumber(y);
+perfectNumber(z);
