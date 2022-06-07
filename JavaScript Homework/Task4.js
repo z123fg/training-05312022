6145a56aee941765513e79a1279ab7ecd21ce4b8
dachar1 <acharya.david@outlook.com>
2022-06-07 12:10:13 -0400

    Create Task4.js

---
+++ JavaScript Homework/Task4.js
@@ -0,0 +1,10 @@
+// Write a JavaScript function that returns a passed string with letters in alphabetical order. 
+// Example string: 'webmaster' 
+// Expected Output: 'abeemrstw'
+// Assume punctuation and numbers symbols are not included in the passed string.
+
+
+function alphaorder(str){
+    return str.split('').sort().join('');
+}
+console.log(alphaorder("webmaster"));
