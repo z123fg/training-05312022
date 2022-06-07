c1e6701f7c0fab7169b96ff320f0a1f9e25bfe97
aleimew <alex@amoore.com>
2022-06-07 12:53:25 -0400

    Did function 27

--- javascript/function27.js
+++ javascript/function27.js
@@ -1,7 +1,61 @@
 /**function 27 */
 function longestPalidrome(x)
 {
+    var str = x;
+    var palSubstr = "";
+    var lonPalSubstr = "";
+    var palChars = [];
 
+    for (var i = 0; i < str.toString().length; i++)
+    {
+        var char = str.slice(i, i+1);
+
+        if(palChars.length === 0 || 
+            palChars.length === 1)
+        {
+            palChars.push(char);
+            //console.log(palChars);
+        }
+        else
+        {
+            if (char === palChars[0])
+            {
+                if (palSubstr.length === 0)
+                {
+                    palSubstr += palChars[0].toString();
+                    palSubstr += palChars[1].toString();
+                    palSubstr += char;
+                }
+                else
+                {
+                    palSubstr += char;
+                }
+            }
+            else
+            {
+                if (lonPalSubstr.length < palSubstr.length)
+                {
+                    lonPalSubstr = palSubstr
+                }
+                palSubstr = "";
+            }
+            palChars[0] = palChars[1];
+            palChars[1] = char;
+            //console.log(palChars);
+        }
+        
+    }
+
+    if (lonPalSubstr.length < palSubstr.length)
+    {
+        lonPalSubstr = palSubstr
+    }
+
+    console.log(lonPalSubstr);
 }
 
 console.log("function 27: ");
+var x = "babananana";
+var y = "abracadabra";
+longestPalidrome(x);
+longestPalidrome(y);
