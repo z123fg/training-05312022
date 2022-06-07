2852772bb9c77cf68cd6c135907b84956848119b
aleimew <alex@amoore.com>
2022-06-07 14:18:11 -0400

    did function 28

--- javascript/function28.js
+++ javascript/function28.js
@@ -1,7 +1,13 @@
 /**function 28 */
 function javascriptFunction(x)
 {
+    console.log(`This function reads functions, and the name of this function is ${x}`);
+}
 
+function exFunct(){
+    console.log("This is a function");
 }
 
 console.log("function 28: ");
+const functObj = exFunct.name;
+javascriptFunction(functObj);
