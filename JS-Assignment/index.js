302ae6c57ac197cc7dae7deb95ff1d7559e38828
Johnathan238 <jonathanumana86@gmail.com>
2022-06-07 11:02:39 -0400

    Will complete JS-Assignment/Homework tonight

--- JS-Assignment/index.js
+++ JS-Assignment/index.js
@@ -1,8 +1,38 @@
 // 1.
-function reverseNum(num) {
-  return (parseFloat(
-    num.toString().split('').reverse().join('')
-  )* Math.sign(num))
-}
+// function reverseNum(num) {
+//   return (parseFloat(
+//     num.toString().split('').reverse().join('')
+//   )* Math.sign(num))
+// }
 
-console.log(reverseNum(543.21))
+// console.log(reverseNum(543.21))
+
+// 2.
+// function isPalindrome(string) {
+//   const arrValues = string.split('')
+
+//   const reverseArr = arrValues.reverse()
+
+//   const reverseString = reverseArr.join('')
+
+//   if (string == reverseString) {
+//     console.log('It/s a Palindrome');
+//   } else {
+//     console.log('It/s not a Palindrome');
+//   }
+// }
+
+// const string = prompt('Enter a String !')
+// isPalindrome(string)
+
+// 3. 
+let generateComb = (str) => {
+  let combination = []
+  for (let i = 0; i < str.length; i++){
+    for (let j = i + 1; j < str.length + 1; j++){
+      combination.push(str.slice(i , j))
+    }
+  }
+  return combination
+}
+console.log(generateComb('Dog'))
