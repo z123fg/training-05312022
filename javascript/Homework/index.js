7b9d287841d0c1bfb8056e0a57a415e675db0fae
ting-hu <huxu@uwplatt.edu>
2022-06-07 12:55:24 -0500

    There are 5 questions that need more time to complete 9,14,17, 26, and 27

--- javascript/Homework/index.js
+++ javascript/Homework/index.js
@@ -17,7 +17,7 @@ function qOne(input){
     //      return input;
     // }
 }
-console.log(qOne("32245"));
+console.log("Question 1: " + qOne("32245"));
 
 //Question 2:
 function qTwo(input){
@@ -31,7 +31,7 @@ function qTwo(input){
     else
         return false;
 }
-console.log(qTwo("nurses run"));
+console.log("Question 2: " + qTwo("nurses run"));
 
 //Question 3:
 function qThree(input){
@@ -44,7 +44,7 @@ function qThree(input){
     }
     return output;
 }
-console.log(qThree("dog"));
+console.log("Question 3: " + qThree("dog"));
 
 //Question 4:
 function qFour(input){
@@ -53,7 +53,7 @@ function qFour(input){
     input = input.join("");
     return input;
 }
-console.log(qFour("webmaster"));
+console.log("Question 4: " + qFour("webmaster"));
 
 //Question 5:
 function qFive(input){
@@ -64,7 +64,7 @@ function qFive(input){
     }
     return output;
 }
-console.log(qFive("the quick brown fox"));
+console.log("Question 5: " + qFive("the quick brown fox"));
 
 //Question 6:
 function qSix(input){
@@ -77,7 +77,7 @@ function qSix(input){
     }
     return output;
 }
-console.log(qSix("Web Development Tutorial"));
+console.log("Question 6: " + qSix("Web Development Tutorial"));
 
 //Question 7:
 function qSeven(input){
@@ -90,7 +90,7 @@ function qSeven(input){
     }
     return counter;
 }
-console.log(qSeven("The quick brown fox"));
+console.log("Question 7: " + qSeven("The quick brown fox"));
 
 //Question 8:
 function qEight(input){
@@ -107,7 +107,7 @@ function qEight(input){
         return true;
     }
 }
-console.log(qEight(9));
+console.log("Question 8: " + qEight(9));
 
 //Question 9: --Not working
 // function qNine(input){
@@ -122,6 +122,7 @@ console.log(qEight(9));
 
 //Question 10:
 function qTen(){
+    console.log("Question 10: ");
     for(let i = 0; i < 4; i++){
         for(let j = 0; j < 4; j++){
             if(i == j)
@@ -141,7 +142,7 @@ function qTOne(input){
     output.push(input[input.length-2]);
     return output;
 }
-console.log(qTOne([1,3,4,5,2]));
+console.log("Question 11: " + qTOne([1,3,4,5,2]));
 
 //Question 12:
 function qTTwo(input){
@@ -162,7 +163,7 @@ function qTTwo(input){
         return false;
 }
 
-console.log(qTTwo(28));
+console.log("Question 12: " + qTTwo(28));
 
 //Question 13:
 function qTThree(input){
@@ -179,13 +180,13 @@ function qTThree(input){
         return "Not a positive number."
 }
 
-console.log(qTThree(9));
+console.log("Question 13: " + qTThree(9));
 
 //Question 14: Don't understand the question 
 function qTFour(){
 
 }
-//console.log(qTFour());
+//console.log("Question 14: " + qTFour());
 
 //Question 15:
 function qTFive(b, n){
@@ -198,9 +199,9 @@ function qTFive(b, n){
     return output;
 }
 
-console.log(qTFive(2,3));
+console.log("Question 15: " + qTFive(2,3));
 
-//Question 16: Don't understand the question 
+//Question 16: Don't understand the question but completed
 function qTSix(input){
     var output="";
     for (var i = 0;i < input.length; i++)
@@ -210,8 +211,154 @@ function qTSix(input){
     }
   return output;  
 }
-console.log(qTSix("thequickbrownfoxjumpsoverthelazydog"));
+console.log("Question 16: " + qTSix("thequickbrownfoxjumpsoverthelazydog"));
 
+//Question 17: Not sure what to do...
 function qTSeven(){
-    
+
 }
+
+//Question 18:
+function qTEight(input, key){
+    let l = 0;
+    let r = input.length - 1;
+    let mid;
+    while (r >= l) {
+         mid = l + Math.floor((r - l) / 2);
+  
+        if (input[mid] == key)
+            return "The index of the search is " + mid;
+  
+        if (input[mid] > key)
+            r = mid - 1;
+        else
+            l = mid + 1;
+    }
+    return "The element is not in the list.";
+}
+console.log("Question 18: " + qTEight([3, 4, 9, 10, 17, 30], 9));
+
+//Question 19:
+function qTNine(input, key){
+    var output = [];
+    for(let i = 0; i < input.length; i++){
+        if(input[i] >= key)
+            output.push(input[i]);
+    }
+    
+    if(output.length > 1)
+        return output;
+    else 
+        return "No element in the list is larger than the key.";
+}
+console.log("Question 19: " + qTNine([3, 4, 9, 10, 17, 30], 41));
+
+//Question 20:
+function qTwenty(length){
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var output = '';
+    for ( let i = 0; i < length; i++ ) {
+        output += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+
+    return output;
+}
+console.log("Question 20: " + qTwenty(7));
+
+//Question 21: Only subset for 2... Need to upgrade
+function qTwOne(input){
+    let output = []
+    for (let i = 0; i < input.length; i++) {
+        for (let j = i + 1; j < input.length; j++) {
+            output.push([input[j], input[i]]);
+        }
+    }
+    return output;
+}
+console.log("Question 21: " + qTwOne([1,2,3]));
+
+//Question 22:
+function qTwTwo(input,key){
+    var output = 0;
+    for(let i = 0; i < input.length; i++){
+        if(input[i] == key)
+            output++;
+    }
+    return output;
+}
+console.log("Question 22: " + qTwTwo('microsoft.com','o'));
+
+//Question 23:
+function qTwThree(input){
+    var output = [];
+    for(let i = 0; i < input.length; i ++){
+        if(input.indexOf(input[i]) == input.lastIndexOf(input[i]))
+            output.push(input[i])
+    }
+
+    if(output.length > 0)
+        return output;
+    else 
+        return "There is no first not repeated character in the string.";
+}
+console.log("Question 23: " + qTwThree('abacddbec'));
+
+//Question 24:
+function qTwFour(input){
+    for(var i = 0; i < input.length; i++)
+    { 
+        for(var j = 0; j < ( input.length - i -1 ); j++)
+        {
+          if(input[j] < input[j+1])
+          {
+            var temp = input[j];
+            input[j] = input[j + 1];
+            input[j+1] = temp;
+          }
+        }
+      }
+    return input;
+}
+console.log("Question 24: " + qTwFour([12, 345, 4, 546, 122, 84, 98, 64, 9, 1, 3223, 455, 23, 234, 213]));
+
+//Question 25:
+function qTwFive(input){
+    var output = "";
+    for(let i = 0; i < input.length-1; i ++){
+        if(input[i].length > input[i+1].length)
+            output = input[i];
+        else
+            output = input[i+1];
+    }
+
+    return output;
+}
+console.log("Question 25: " + qTwFive(["Australia", "Germany", "United States of America"]));
+
+//Question 26: Not sure how to approach
+function qTwSix(){
+
+}
+
+//Question 27: Not sure how to approach
+function qTwSeven(){
+
+}
+
+//Question 28:
+function another(){
+    return "In the another.";
+}
+function qTwEight(){
+    return another();
+}
+console.log("Question 28: " + qTwEight());
+
+//Question 29:
+function qTwNine() {
+    return arguments.callee.name;
+}
+console.log("Question 29: " + qTwNine());
+
+
+
