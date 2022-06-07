d2b9086d4904ea92bd2fb51e5f889edb08cc043a
dachar1 <acharya.david@outlook.com>
2022-06-07 13:00:32 -0400

    Create Task9.js

---
+++ JavaScript Homework/Task9.js
@@ -0,0 +1,12 @@
+// Write a JavaScript function which accepts an argument and returns the type. 
+// Note: There are six possible values that typeof returns: object, boolean, function, number, string,
+// and undefined
+ const variableType = (str) =>{
+     console.log(typeof(str));
+ }
+ variableType(1);
+ variableType("Javascript");
+ variableType(true);
+ variableType(null);
+ variableType(Object);
+ variableType(undefined);
