97e61ae074dfe941e192e1ff379398da0ae24f7f
aleimew <alex@amoore.com>
2022-06-07 14:19:54 -0400

    Just realized I actually did function 29.

--- javascript/function29.js
+++ javascript/function29.js
@@ -1,7 +1,13 @@
 /**function 29 */
 function functionName(x)
 {
+    console.log(`This function reads functions, and the name of this function is ${x}`);
+}
 
+function exFunct(){
+    console.log("This is a function");
 }
 
 console.log("function 29: ");
+const functObj = exFunct.name;
+functionName(functObj);
