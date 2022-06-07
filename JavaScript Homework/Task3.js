10b60f6e586d314a0b50e0627c79dcaa3c1bcf5a
dachar1 <acharya.david@outlook.com>
2022-06-07 12:10:11 -0400

    Create Task3.js

---
+++ JavaScript Homework/Task3.js
@@ -0,0 +1,9 @@
+// Write a JavaScript function that generates all combinations of a string. 
+// Example string: 'dog' 
+// Expected Output: d, do, dog, o, og, g
+
+function combination(word)
+{
+    
+}
+combination('dog')
