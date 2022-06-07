e119c4110f65ea053f2e6a4ab413f617d83742c9
dachar1 <acharya.david@outlook.com>
2022-06-07 15:01:56 -0400

    Create Task11.js

---
+++ JavaScript Homework/Task11.js
@@ -0,0 +1,8 @@
+// Write a JavaScript function which will take an array of numbers stored and find the second
+// lowest and second greatest numbers, respectively. 
+// Sample array: [1,2,3,4,5]
+// Expected Output: 2,4
+
+const greatestarray = (array) =>{
+    
+}
