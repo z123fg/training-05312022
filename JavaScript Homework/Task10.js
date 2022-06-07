6502fa01d6ed74b85b990b99ce4024353284b372
dachar1 <acharya.david@outlook.com>
2022-06-07 15:01:59 -0400

    Update Task10.js

--- JavaScript Homework/Task10.js
+++ JavaScript Homework/Task10.js
@@ -1,1 +1,23 @@
-Write a JavaScript function which returns the n rows by n columns identity matrix. 
+//Write a JavaScript function which returns the n rows by n columns identity matrix. 
+const matrix = (num) =>{
+    
+    for(var i=0;i<num;i++)
+    {
+        
+        for(var j=0; j<num; j++)
+        {
+            
+            if (i===j)
+            {
+                console.log('1')
+            }
+            else
+            {
+                console.log('0');
+            }
+            
+        }
+       console.log('======');
+    }
+}
+matrix(5);
