66083498d0ace559d00f8cfade9d566d0532c1cc
ting-hu <huxu@uwplatt.edu>
2022-06-06 23:42:13 -0500

    Its too late, will come back tmr for more

--- javascript/Homework/index.js
+++ javascript/Homework/index.js
@@ -17,7 +17,6 @@ function qOne(input){
     //      return input;
     // }
 }
-
 console.log(qOne("32245"));
 
 //Question 2:
@@ -32,7 +31,6 @@ function qTwo(input){
     else
         return false;
 }
-
 console.log(qTwo("nurses run"));
 
 //Question 3:
@@ -46,7 +44,6 @@ function qThree(input){
     }
     return output;
 }
-
 console.log(qThree("dog"));
 
 //Question 4:
@@ -56,7 +53,6 @@ function qFour(input){
     input = input.join("");
     return input;
 }
-
 console.log(qFour("webmaster"));
 
 //Question 5:
@@ -68,7 +64,6 @@ function qFive(input){
     }
     return output;
 }
-
 console.log(qFive("the quick brown fox"));
 
 //Question 6:
@@ -82,7 +77,6 @@ function qSix(input){
     }
     return output;
 }
-
 console.log(qSix("Web Development Tutorial"));
 
 //Question 7:
@@ -96,16 +90,128 @@ function qSeven(input){
     }
     return counter;
 }
-
 console.log(qSeven("The quick brown fox"));
 
 //Question 8:
 function qEight(input){
-    if(input === '1'){
+    if(input < 4){
         return false;
-    }else if(input > 1){
-        
+    }else{
+        for(var x = 2; x < input; x++)
+        {
+            if(input % x === 0)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
+console.log(qEight(9));
 
-console.log(qEight(5));
+//Question 9: --Not working
+// function qNine(input){
+//     // for(let i = 0; i < input.length; i++){
+//     //     if(typeof input[i] == )
+//     // }
+
+//     if(typeof input === Number)
+//         console.log("yes");
+// }
+// console.log(qNine(12));
+
+//Question 10:
+function qTen(){
+    for(let i = 0; i < 4; i++){
+        for(let j = 0; j < 4; j++){
+            if(i == j)
+                console.log("1");
+            else
+                console.log('0');
+        }
+    }
+}
+qTen();
+
+//Question 11:
+function qTOne(input){
+    input.sort();
+    var output = [];
+    output.push(input[1]);
+    output.push(input[input.length-2]);
+    return output;
+}
+console.log(qTOne([1,3,4,5,2]));
+
+//Question 12:
+function qTTwo(input){
+    var output = [];
+    for(let i = 0; i < input; i ++){
+        if(input % i == 0)
+            output.push(i);
+    }
+
+    var sum = 0;
+    for(let i = 0; i < output.length; i ++){
+        sum += output[i];
+    }
+
+    if(input == sum)
+        return true;
+    else 
+        return false;
+}
+
+console.log(qTTwo(28));
+
+//Question 13:
+function qTThree(input){
+    if(input > 0 )
+    {
+        var output = [];
+        for(let i = 0; i < input; i ++){
+            if(input % i == 0)
+                output.push(i);
+        }
+        return output;
+    }
+    else
+        return "Not a positive number."
+}
+
+console.log(qTThree(9));
+
+//Question 14: Don't understand the question 
+function qTFour(){
+
+}
+//console.log(qTFour());
+
+//Question 15:
+function qTFive(b, n){
+    var output = 1;
+    for (var i = 1; i <= n; i++)
+    {
+        output = b * output;        
+    }
+        
+    return output;
+}
+
+console.log(qTFive(2,3));
+
+//Question 16: Don't understand the question 
+function qTSix(input){
+    var output="";
+    for (var i = 0;i < input.length; i++)
+    {
+        if(output.indexOf(input.charAt(i)) == -1)
+            output += input[i];  
+    }
+  return output;  
+}
+console.log(qTSix("thequickbrownfoxjumpsoverthelazydog"));
+
+function qTSeven(){
+    
+}
