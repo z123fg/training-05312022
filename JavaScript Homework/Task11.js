47b1d5aa174f333204a6d9d6e692b24f4900f2b9
dachar1 <acharya.david@outlook.com>
2022-06-08 14:16:00 -0400

    Update Task11.js

--- JavaScript Homework/Task11.js
+++ JavaScript Homework/Task11.js
@@ -3,6 +3,10 @@
 // Sample array: [1,2,3,4,5]
 // Expected Output: 2,4
 
-const greatestarray = (array) =>{
-    array = []
-}
+const greatestarray = (arr) =>{
+if (arr.length < 2) return;
+  arr.sort((a, b) => a - b);
+  console.log("The second lowest number is ", arr[1]);
+  console.log("The Second highest number is ", arr[arr.length - 2]);
+};
+greatestarray([1,2,3,4,5])
