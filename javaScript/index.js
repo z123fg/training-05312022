3f18896214eda52b934722bc5b5459016f63b411
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2022-06-08 00:19:40 -0500

    hw3

--- javaScript/index.js
+++ javaScript/index.js
@@ -1,10 +1,9 @@
 // 1. reverse a number.
 function reverse(number) {
-  if(typeof number === 'number'){
+  if (typeof number === "number") {
     return Number(number.toString().split("").reverse().join(""));
-  }
-  else if(typeof number === 'string'){
-    return number.split("").reverse().join("")
+  } else if (typeof number === "string") {
+    return number.split("").reverse().join("");
   }
 }
 // test question 1
@@ -20,9 +19,27 @@ console.log(palindrome("helleh"));
 
 // question 3 generates all combinations of a string
 function combinationsOfaString(string) {
+  list_of_arr = [];
+  for (let i = 0; i < string.length; i++) {
+    for (let j = i + 1; j < string.length + 1; j++) {
+      list_of_arr.push(string.slice(i, j));
+    }
+  }
+  return list_of_arr;
+}
+// test question 3
+console.log(combinationsOfaString("dog"));
 
-}
-// question 4 returns a passed string with letters in alphabetical order. 
+// question 4 returns a passed string with letters in alphabetical order.
 function orderString(string) {
-  
+  return string.split("").sort().join("");
 }
+// test question 4
+console.log(orderString("daegbcf"));
+
+// question 5 Write a function that accepts a string as a parameter and converts the first letter of each word of the string in upper case. 
+function capitalALetterInFirstWord(string){
+  return string.split(" ").map(word => word[0].toUpperCase() + word.slice(1)).join(" ");
+}
+// test question 5
+console.log(capitalALetterInFirstWord("how are you?"));
