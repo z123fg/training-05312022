7a793c6347a9d8d44944efad2c2b59dbd250c78b
dachar1 <acharya.david@outlook.com>
2022-06-08 14:16:04 -0400

    Create Task12.js

---
+++ JavaScript Homework/Task12.js
@@ -0,0 +1,13 @@
+const isPerfectNum = (num) => {
+    if (num <= 1) return false;
+    let divisor = ~~(num / 2);
+    let sum = 0;
+    while (divisor >= 1) {
+      if (num % divisor === 0) {
+        sum += divisor;
+      }
+      divisor--;
+    }
+    return sum === num;
+  };
+  
