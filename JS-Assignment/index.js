1a7be320e8418b24a0b670ea6d48734ee9c947b2
Johnathan238 <jonathanumana86@gmail.com>
2022-06-09 00:39:31 -0400

    Need just 10 more to finish up

--- JS-Assignment/index.js
+++ JS-Assignment/index.js
@@ -162,3 +162,198 @@
 //      }
 //  }
 // matrix(4);
+
+// 11 .
+// function Second_Greatest_Lowest(arr_num)
+// {
+//   arr_num.sort(function(x,y)
+//            {
+//            return x-y;
+//            });
+//   var uniqa = [arr_num[0]];
+//   var result = [];
+  
+//   for(var j=1; j < arr_num.length; j++)
+//   {
+//     if(arr_num[j-1] !== arr_num[j])
+//     {
+//       uniqa.push(arr_num[j]);
+//     }
+//   }
+//     result.push(uniqa[1],uniqa[uniqa.length-2]);
+//   return result.join(',');
+//   }
+
+// console.log(Second_Greatest_Lowest([1,2,3,4,5]));
+
+// 12 .
+// function is_perfect(number)
+// {
+// var temp = 0;
+//    for(var i=1;i<=number/2;i++)
+//      {
+//          if(number%i === 0)
+//           {
+//             temp += i;
+//           }
+//      }
+   
+//      if(temp === number && temp !== 0)
+//         {
+//        console.log("It is a perfect number.");
+//         } 
+//      else
+//         {
+//        console.log("It is not a perfect number.");
+//         }   
+//  } 
+// is_perfect(28);
+
+// 13 . 
+// function factors(n)
+// {
+//  var num_factors = [], i;
+ 
+//  for (i = 1; i <= Math.floor(Math.sqrt(n)); i += 1)
+//   if (n % i === 0)
+//   {
+//    num_factors.push(i);
+//    if (n / i !== i)
+//     num_factors.push(n / i);
+//   }
+//  num_factors.sort(function(x, y)
+//    {
+//      return x - y;});  // numeric sort
+//      return num_factors;
+//     }
+// console.log(factors(15));  // [1,3,5,15] 
+// console.log(factors(16));  // [1,2,4,8,16] 
+// console.log(factors(17));  // [1,17]
+
+// 14 .
+// function amountTocoins(amount, coins) 
+// {
+//  if (amount === 0) 
+//   {
+//      return [];
+//    } 
+//  else
+//    {
+//      if (amount >= coins[0]) 
+//        {
+//         left = (amount - coins[0]);
+//         return [coins[0]].concat( amountTocoins(left, coins) );
+//         } 
+//       else
+//         {
+//          coins.shift();
+//          return amountTocoins(amount, coins);
+//         }
+//     }
+// } 
+// console.log(amountTocoins(46, [25, 10, 5, 2,1]));
+
+// 15 .
+// function exp(b,n)
+// {
+//         var ans = 1;
+//         for (var i =1; i <= n; i++)
+//         {
+//                 ans = b * ans;        
+//         }
+//         return ans;
+// }
+// console.log(exp(2, 3));
+
+// 16 .
+// function unique_char(str1)
+// {
+//  var str=str1;
+//  var uniql="";
+//  for (var x=0;x < str.length;x++)
+//  {
+
+//  if(uniql.indexOf(str.charAt(x))==-1)
+//   {
+//   uniql += str[x];  
+  
+//    }
+//   }
+//   return uniql;  
+// }  
+// console.log(unique_char("thequickbrownfoxjumpsoverthelazydog"));
+
+// 17 .
+// function count(string) {
+//   let string1 = string.split("").sort().join("");
+//   let counter = 1;
+//   for (let i = 0; i < string.length; i++) {
+//     if (string1[i] == string1[i + 1]) {
+//       counter++;
+//     } else {
+//       console.log(string1[i] + " " + counter);
+//       counter = 1;
+//     }
+//   }
+// }
+// count("thequickbrownfoxjumpsoverthelazydog");
+
+// 18 .
+// function array_binarySearch(narray, delement) {
+//   var mposition = Math.floor(narray.length / 2);
+
+//   if (narray[mposition] === delement){
+//      return mposition;
+//   }
+//   else if (narray.length === 1) 
+//   {
+//      return null;
+//   }
+//   else if (narray[mposition] < delement) {
+//      var arr = narray.slice(mposition + 1);
+//      var res = array_binarySearch(arr, delement);
+//      if (res === null)
+//      {
+//         return null;
+//      }
+//      else {
+//         return mposition + 1 + res;
+//      }
+//   }
+//   else {
+//      var arr1 = narray.slice(0, mposition);
+//      return array_binarySearch(arr1, delement);
+//   }
+// }
+
+// var myArray = [1, 2, 3, 5, 6, 7, 10, 11, 14, 15, 17, 19, 20, 22, 23];
+// console.log(array_binarySearch(myArray, 6));
+
+// 19 .
+// let returnLarger = (arr, num) => {
+//   return arr.reduce((acc, curr)=>{  
+//     if(curr > num){
+//       return acc.concat(curr)  // Concatenate the acc with arr
+//     }else{
+//       return acc
+//     }
+//   }, [])   // Initialize the accumulator with an empty array
+// }
+  
+  
+// console.log(returnLarger( [65, 16, 0, 6, 64, 1, 68], 16))
+// console.log(returnLarger([6, 46, 54, 6, 56, 54, 65, 4, 65], 50))
+
+// 20 .
+// function makeid(l)
+// {
+// var text = "";
+// var char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+// for(var i=0; i < l; i++ )
+// {  
+// text += char_list.charAt(Math.floor(Math.random() * char_list.length));
+// }
+// return text;
+// }
+// console.log(makeid(8));
+
