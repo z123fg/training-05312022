51cb6ea7b45175db450914ec56d0a6c969997bca
Ming Zhao Li <mli63@buffalo.edu>
2022-06-10 17:12:06 -0400

    Final Assessment 06102022, Did not finish edit/status change, Able to create input box for edit but having trouble submitting value

---
+++ finalAssessment_06102022/index.js
@@ -0,0 +1,195 @@
+const APIs = (() => {
+    const URL = "http://localhost:3000/todos"
+
+    const addTodo = (newTodos) => {
+        return fetch(URL, {
+            method: "POST",
+            body: JSON.stringify(newTodos),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            return res.json();
+        })
+    }
+
+    const deleteTodo = (id) => {
+        return fetch(`${URL}/${id}`, {
+            method: "DELETE"
+        }).then((res) => {
+            return res.json();
+        })
+    }
+
+    const editTodo = (newTodo, id) => {
+        return fetch(`${URL}/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(newTodo),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then((res) => {
+            return res.json();
+        })
+    }
+
+    const changeTodoStatus = (status, id) => {
+
+    }
+
+    const getTodos = () => {
+        return fetch(`${URL}`).then((res) => {
+            return res.json();
+        })
+    }
+
+    return {
+        getTodos,
+        addTodo,
+        deleteTodo,
+        editTodo,
+        changeTodoStatus
+    }
+    
+})();
+
+
+
+const Model = (() => {
+    class State {
+        #todos;
+        #onChangeCb;
+        constructor() {
+            this.#todos = [];
+            this.#onChangeCb = () => { }
+        }
+
+        get todos() {
+            return this.#todos;
+        }
+
+        set todos(newTodos) {
+            this.#todos = newTodos;
+            this.#onChangeCb();
+        }
+
+        subscribe = (cb) => {
+            this.#onChangeCb = cb;
+        }
+    }
+
+    return {
+        State
+    }
+
+})();
+
+
+
+const View = (() => {
+    const formEl = document.querySelector(".todo__form");
+    const todoListEl = document.querySelector(".todo__list");
+    const renderTodolist = (todos) => {
+        let template = "";
+        todos.sort((a,b) => b.id - a.id).forEach((todo) => {
+            template += `
+                <li><span class="span-${todo.id}" id="${todo.id}">${todo.content}</span><button class="btn--edit" id="${todo.id}">Edit</button><button class="btn--delete" id="${todo.id}">Delete</button></li>
+            `
+        })
+        todoListEl.innerHTML = template;
+    }
+
+    return {
+        formEl,
+        todoListEl,
+        renderTodolist
+    }
+
+})();
+
+
+
+const ViewModel = ((Model, View) => {
+    const state = new Model.State();
+
+    const addTodo = () => {
+        View.formEl.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const content = event.target[0].value;
+            if(content.trim() === "")return;
+            const newTodos = { content , status: false };
+            APIs.addTodo(newTodos).then(res => {
+                console.log("Result: ", res);
+                state.todos = [res, ...state.todos];
+            })
+
+        })
+    }
+
+    const deleteTodo = () => {
+        View.todoListEl.addEventListener("click", (event) => {
+            const { id } = event.target;
+            if (event.target.className === "btn--delete") {
+                APIs.deleteTodo(id).then(res => {
+                    console.log("Result: ", res);
+                    state.todos = state.todos.filter((todo) => {
+                        return +todo.id !== +id;
+                    });
+                })
+            }
+        })
+    }
+
+    const changeToEdit = () => {
+        View.todoListEl.addEventListener("click", (event) => {
+            // console.log(event.currentTarget, event.target);
+            
+            const { id } = event.target;
+            let spanEl = document.querySelector(`.span-${id}`);
+            let spanContent = spanEl.innerHTML;
+            console.log(spanEl);
+
+            if (event.target.className === "btn--edit" && spanEl.id === id && !spanEl.classList.contains("text--editable")) {
+                spanEl.innerHTML = `<input value=${spanContent}>`;
+                spanEl.classList.toggle("text--editable");
+            }else if (event.target.className === "btn--edit" && spanEl.id === id && spanEl.classList.contains("text--editable")) {
+                console.log("editable");
+                spanEl.innerHTML = spanEl.value;
+                spanEl.classList.toggle("text--editable");
+            }
+        })
+    }
+
+    const editTodo = () => {
+        
+    }
+
+    const changeTodoStatus = () => {
+
+    }
+
+    const getTodos = () => {
+        APIs.getTodos().then(res => {
+            state.todos = res;
+        })
+    }
+
+    const bootstrap = () => {
+        addTodo();
+        deleteTodo();
+        changeToEdit();
+        editTodo();
+        changeTodoStatus();
+        getTodos();
+        state.subscribe(() => {
+            View.renderTodolist(state.todos)
+        });
+    }
+
+    return {
+        bootstrap
+    }
+
+})(Model, View);
+
+ViewModel.bootstrap();
