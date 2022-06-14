8d2be3e8698b9c67fd8d999aadbecf0a5c1ba469
Jiawen Zhu <jzhu10@mail.sfsu.edu>
2022-06-14 08:50:02 -0500

    style fix

--- final_evaluation/index.js
+++ final_evaluation/index.js
@@ -1,8 +1,3 @@
-const EDITICON =  '<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></svg>'
-
-const DELETEICON = '<svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small"><path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path></svg>'
-
-
 /* 
   closure, IIFE
   event bubbling, event capturing
