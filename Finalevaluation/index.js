b0da36552145c96ff9fa0562930dec0f19cfb6e8
Xiaohei2021 <hualiang.tian@gmail.com>
2022-06-22 14:50:48 -0400

    tweaks

--- Finalevaluation/index.js
+++ Finalevaluation/index.js
@@ -97,8 +97,25 @@ const View = (() => {
             tasks += 
             `<li>
                 <span> ${todo.content}</span>
-                <button class="btn--delete" id="${todo.id}">Delete</button>
-                <button class="btn--edit" id="${todo.id}">Edit</button>
+               
+                
+                <!--<button class="btn--edit" id="${todo.id}">Edit</button> 
+                <button class="btn--delete" id="${todo.id}">Delete</button>-->
+
+                <button class="btn--edit" id="${todo.id}">
+                    <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="EditIcon" aria-label="fontSize small">
+                        <path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path>
+                    </svg>
+                </button>
+
+                <button class="btn--delete" id="${todo.id}">
+                    <svg focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="DeleteIcon" aria-label="fontSize small">
+                        <path d="M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"></path>
+                    </svg>
+                </button>
+                
+
+
             </li>`
         })
         eventList.innerHTML = tasks;
@@ -157,9 +174,34 @@ const ViewModel = ((Model, View) => {
             }
 
 
-            if (event.target.innerText === "Edit") {
+            // if (event.target.innerText === "Edit") {
+            //     // console.log(event.target.parentElement)
+            //     event.target.innerText = "Save";
+            //     displayEditField();
+            // }
+
+            // else if (event.target.className === "btn--delete") {
+            //     const id = event.target.id
+            //     APIs.deleteTodo(id).then(res => {
+            //         console.log("Res", res);
+            //         state.todos = state.todos.filter((todo) => {
+            //             return +todo.id !== +id
+            //         });
+            //     });
+            // }
+
+            // else if (event.target.innerText ==="Save"){
+            //     console.log("save")
+            //     event.target.innerText="Edit";
+            //     updateTask();
+            // }
+
+            if (event.target.className === "btn--edit") {
+                // console.log(event)
+                // debugger
                 // console.log(event.target.parentElement)
-                event.target.innerText = "Save";
+                // event.target.innerText = "Save";
+
                 displayEditField();
             }
 
@@ -173,11 +215,11 @@ const ViewModel = ((Model, View) => {
                 });
             }
 
-            else if (event.target.innerText ==="Save"){
-                console.log("save")
-                event.target.innerText="Edit";
-                updateTask();
-            }
+            // else if (event.target.className ==="btn--edit"){
+            //     console.log("save")
+            //     event.target.innerText="Edit";
+            //     updateTask();
+            // }
 
         })
     }
