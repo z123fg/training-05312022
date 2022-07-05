dcb7e2b886510e8e2ab94c59035d484fcc68a191
aleimew <alex@amoore.com>
2022-07-05 17:06:04 -0400

    attempted to create handeler for clearing all

--- component_practice/src/components/List.js
+++ component_practice/src/components/List.js
@@ -1,39 +1,32 @@
 import React, {useState} from "react";
 
-function ClearAll() {
-    const [setChecked] = useState(false);
+function List(){
 
-    return (
-        <>
-        <button onClick={() => setChecked((c) => !c)}>Clear all</button>
-        </>
-    )
-}
+    const [checked, setChecked] = useState(false);
 
-class List extends React.Component{
-
-    render(){
-        const {listElement} = this.props;
         return(
             <>
-            <label class="checkbox" id ="c">
-            <input type="checkbox" id="c1"/>
+            <div className="App_top">
+          <p>Selected Value: </p>
+            </div>
+            <br/>
+            <label class="checkbox"  id ="c">
+            <input type="checkbox" checked={checked} onChange={(e) => setChecked(e.currentTarget.checked)} id="c1"/>
             <label for="c1">Thing 1</label>
             <br/><br/>
-            <input type="checkbox" id="c2"/>
+            <input type="checkbox" onChange={(e) => setChecked(e.target.checked)}id="c2"/>
             <label for="c2">Thing 2</label>
             <br/><br/>
-            <input type="checkbox" id="c3"/>
+            <input type="checkbox" onChange={(e) => setChecked(e.target.checked)}id="c3"/>
             <label for="c3">Thing 3</label>
             <br/><br/>
-            <input type="checkbox" id="c4"/>
+            <input type="checkbox" onChange={(e) => setChecked(e.target.checked)}id="c4"/>
             <label for="c4">Thing 4</label>
             <br/><br/>
             </label>
-            <ClearAll/>
+            <button onClick={() => setChecked((c) => !c)}>clear all</button>
             </>
         );
-    }
 }
 
 export default List;
