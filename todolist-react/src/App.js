9726da3658ce8513f6d49275df84415580ec1867
AdamZhou1029 <yizhouzhou@yizhous-MacBook-Pro.local>
2022-07-06 15:48:52 -0500

    todolist react

--- todolist-react/src/App.js
+++ todolist-react/src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import Counter from './components/Counter';
 import { useState } from 'react';
+import Todolist from './components/Todolist/Todolist';
 
 function App() {
   const [showCounter, setShowCounter] = useState(true);
   return (
     <div className="App">
-      <button onClick={()=>{setShowCounter(prev=>!prev)}}>destroy counter</button>
-      {showCounter&&<Counter/>}
+      {/* <button onClick={()=>{setShowCounter(prev=>!prev)}}>destroy counter</button>
+      {showCounter&&<Counter/>} */}
+      <Todolist/>
     </div>
   );
 }
