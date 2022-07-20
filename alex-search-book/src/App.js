dddb5143e53bc8ae28ce122e759aef9eca885479
aleimew <alex@amoore.com>
2022-07-19 21:00:56 -0400

    turns out looking through the code after pulling in the example with the router implimented is going to be harder that I thought, so I'll continue this tommorow.

--- alex-search-book/src/App.js
+++ alex-search-book/src/App.js
@@ -1,27 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import { Provider } from 'react-redux';
-import {store } from './redux/store'
-import Wishlist from './pages/Wishlist/Wishlist';
+import { store } from "./redux/store";
+import { useState } from "react";
+import Header from "./components/Header/Header";
+import Wishlist from "./pages/Wishlist/Wishlist";
+import Home from "./pages/Home/Home";
+import MyBrowserRouter from '../../search-book/src/router-demo/MyBrowserRouter';
+import MyProvider from '../../search-book/src/redux-demo/MyProvider';
+import MyRoutes from '../../search-book/src/router-demo/MyRoutes';
+import MyRoute from '../../search-book/src/router-demo/MyRoute';
 
 function App() {
-  const [curPage, serCurPage] = useState("home");
+  const [curPage, setCurPage] = useState("home");
 
   const handleChangePage = (page) => {
     setCurPage(page);
-  }
+  };
 
   return (
-    <Provider store={store}>
-      <Header handleChangePage={handleChangePage}/>
-      {
-        curPage==="home"?
-        <Home/>
-        :curPage==="wishlist"?
-        <Wishlist/>
-        :null
-      }
-    </Provider>
+    <div>
+      <MyBrowserRouter>
+        <MyProvider>
+          <Header handleChangePage={handleChangePage}/>
+
+          <MyRoutes>
+            <MyRoute path="/" element={<Home/>}/>
+            <MyRoute path="/home" element={<Home />} />
+            <MyRoute path="/wishlist" element={<Wishlist />} />
+          </MyRoutes>
+        </MyProvider>
+      </MyBrowserRouter>
+    </div>
   );
 }
 
